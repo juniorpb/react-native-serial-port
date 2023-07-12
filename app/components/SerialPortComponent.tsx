a1f309279c3561eaa6ce42bfa059affac761e94b
juniorpb <junior.cgjr@gmail.com>
2023-07-12 10:20:07 -0300

    feat: inital commit

---
+++ app/components/SerialPortComponent.tsx
@@ -0,0 +1,62 @@
+import {
+  UsbSerialManager,
+  UsbSerial,
+  Parity,
+} from "react-native-usb-serialport-for-android";
+import { ScrollView, Alert, Button } from "react-native";
+import { useLayoutEffect, useState } from "react";
+
+const SerialPortComponent = () => {
+  const [usbSerial, setUsbSerial] = useState(null);
+  useLayoutEffect(() => {
+    initSerialPort();
+  }, []);
+
+  async function initSerialPort() {
+    try {
+      // check for the available devices
+      const devices = await UsbSerialManager.list();
+
+      if (devices.length < 1) {
+        Alert.alert("Not Devices");
+      }
+      // Send request for the first available device
+      const granted = await UsbSerialManager.tryRequestPermission(
+        devices[0].deviceId
+      );
+
+      if (granted) {
+        // open the port for communication
+        const usbSerialport = await UsbSerialManager.open(devices[0].deviceId, {
+          baudRate: 9600,
+          parity: Parity.None,
+          dataBits: 8,
+          stopBits: 1,
+        });
+        setUsbSerial(usbSerialport);
+      } else {
+        Alert.alert("USB permission denied");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function sendData(data: any) {
+    if (usbSerial) {
+      try {
+        await usbSerial.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+  return (
+    <ScrollView>
+      <Button onPress={() => sendData("0x01")} title="ON" />
+      <Button onPress={() => sendData("0x02")} title="OFF" />
+    </ScrollView>
+  );
+};
+
+export default SerialPortComponent;
